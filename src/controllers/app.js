c0e2052ed4d22a2a40c600ebce7785d9a56cad00
Luis150228 <luis.rangeldiaz@gmail.com>
2021-08-11 19:16:57 -0500

    Se configura el cors y morgan

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import trasladosRoutes from '../routes/traslados'; ///trasladosRoutes fue creado aleatoriamente
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(trasladosRoutes);
 
